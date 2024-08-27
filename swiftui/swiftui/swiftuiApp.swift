e540675eec54435889cb64ffb783590f1ff8f675
abdur999 <abdursrahims@icloud.com>
2024-08-27 21:29:15 +0530

    create sample swiftui project

---
+++ swiftui/swiftui/swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiApp.swift
+//  swiftui
+//
+//  Created by Abdur Rahim on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
